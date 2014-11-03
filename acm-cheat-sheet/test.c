6707aae1cc352f0793443fcfc720b6d5f0dcf4c4
beanmoon <beanmoon24@163.com>
2014-11-03 11:12:28 +0800

    20141103v1

--- acm-cheat-sheet/test.c
+++ acm-cheat-sheet/test.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
+  int i;
+  for(i = 1; i<10; i++){
+    printf("%d\n", i);
+  }
   char const * str = "hello,world";
   //  const char * str = "hello,world";
-  *str = "hi";
-  printf("%s\n",str);
+  const char *s = "wo";
+  printf("%s\n", strstr(str,s));
 }
