96d78c296e2fa3804cc8cf31a418612de12c8aaf
beanmoon <beanmoon24@163.com>
2014-11-24 23:02:06 +0800

    p024binary_tree.c modified

--- acm-cheat-sheet/p024binary_tree.c
+++ acm-cheat-sheet/p024binary_tree.c
@@ -18,7 +18,7 @@ int visit(const tree_node_t *node){
 
 void pre_order_r(const tree_node_t *root,  int (*visit)(const tree_node_t*)){
   if(root == NULL){
-
+    return;
   }
   visit(root);
   pre_order_r(root->left, visit);
@@ -27,7 +27,7 @@ void pre_order_r(const tree_node_t *root,  int (*visit)(const tree_node_t*)){
 
 void in_order_r(const tree_node_t *root,  int (*visit)(const tree_node_t*)){
   if(root == NULL){
-
+    return;
   }
   in_order_r(root->left, visit);
   visit(root);
@@ -36,46 +36,56 @@ void in_order_r(const tree_node_t *root,  int (*visit)(const tree_node_t*)){
 
 void post_order_r(const tree_node_t *root,  int (*visit)(const tree_node_t*)){
   if(root == NULL){
-
+    return;
   }
-  visit(root);
   post_order_r(root->left, visit);
   post_order_r(root->right, visit);
+  visit(root);
 }
 
 int main() {
-  tree_node_t *root;
+  tree_node_t *root = malloc(sizeof(tree_node_t));
   init_btree(root);
   int (*func)(const tree_node_t* root);
   func = visit;
   pre_order_r(root, func);
+  printf("\n");
 }
 
 
 void init_btree(tree_node_t *root){
-  root->value = 'A';
-  tree_node_t * p1 = (tree_node_t*)malloc(sizeof(tree_node_t));
-  p1->value = '+';
+  root->left = root->right = NULL;
+  root->value = 1;
+  tree_node_t * p1 = malloc(sizeof(tree_node_t));
+  /* p1->left默认值为1，这里一定要显式的声明为null，使其值为0，
+   * 否则会报segmentation fault   */
+  p1->left = NULL;  
+  p1->right = NULL;
+  p1->value = 2;
   root->left = p1;
-  tree_node_t * p2 = (tree_node_t*)malloc(sizeof(tree_node_t));
-  p2->value = '*';
-  p1->left = p2;
-  tree_node_t * p3 = (tree_node_t*)malloc(sizeof(tree_node_t));
-  p3->value = '/';
-  p2->left = p3;
-  tree_node_t * p4 = (tree_node_t*)malloc(sizeof(tree_node_t));
-  p4->value = 'O'; p4->left = p4->right = NULL;
-  p3->left = p4;
-  tree_node_t * q1 = (tree_node_t*)malloc(sizeof(tree_node_t));
-  q1->value = 'G'; q1->left = q1->right = NULL;
-  root->right = q1;
-  tree_node_t * q2 = (tree_node_t*)malloc(sizeof(tree_node_t));
-  q2->value = 'H'; q2->left = q2->right = NULL;
-  p1->right = q2;
-  tree_node_t * q3 = (tree_node_t*)malloc(sizeof(tree_node_t));
-  q3->value = 'I'; q3->left = q3->right = NULL;
-  p2->right = q3;
-  tree_node_t * q4 = (tree_node_t*)malloc(sizeof(tree_node_t));
-  q4->value = 'J'; q4->left = q4->right = NULL;
-  p3->right = q4;
+  tree_node_t * p2 = malloc(sizeof(tree_node_t));
+  p2->left = NULL; 
+  p2->right = NULL;
+  p2->value = 3;
+  root->right = p2;
+  tree_node_t * p3 = malloc(sizeof(tree_node_t));
+  p3->value = 4;
+  p3->left = p3->right = NULL;
+  p1->left = p3;
+  tree_node_t * p4 = malloc(sizeof(tree_node_t));
+  p4->value = 5; 
+  p4->left = p4->right = NULL;
+  p1->right = p4;
+  tree_node_t * p5 = malloc(sizeof(tree_node_t));
+  p5->value = 6; p5->left = p5->right = NULL;
+  p2->left = p5;
+  tree_node_t * p6 = malloc(sizeof(tree_node_t));
+  p6->value = 7; p6->left = p6->right = NULL;
+  p2->right = p6;
+  tree_node_t * p7 = malloc(sizeof(tree_node_t));
+  p7->value = 8; p7->left = p7->right = NULL;
+  p3->left = p7;
+  tree_node_t * p8 = malloc(sizeof(tree_node_t));
+  p8->value = 9; p8->left = p8->right = NULL;
+  p3->right = p8;
 }
